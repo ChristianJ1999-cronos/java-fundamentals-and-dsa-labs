80a1f830b12393893b76a34282e38fb363386595
ChrJimenez <47259794+christianJimenez1999@users.noreply.github.com>
2025-11-11 21:16:09 -0800

    updating two sum | valid anagram solution

--- src/dsa/arrays/TwoSum.java
+++ src/dsa/arrays/TwoSum.java
@@ -1,5 +1,22 @@
 public class TwoSum {
-    public static void main(String[] args){
+    public int[] twoSum(int[] nums, int target) {
 
+        //optimal solution with HASHMAPS -> allows O(1) lookups to check if number exists
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int numNeeded = target - nums[i];
+
+            System.out.println("numNeeded: " + numNeeded + " current number: " + nums[i] + " i: " + i + " target: " + target);
+
+            if(map.containsKey(numNeeded)){
+                return new int[]{map.get(numNeeded), i};
+            }
+
+            map.put(nums[i], i);
+
+        }
+
+        return new int[]{};
     }
 }
--- src/dsa/hashing/ValidAnagram.java
+++ src/dsa/hashing/ValidAnagram.java
@@ -1,5 +1,45 @@
 public class ValidAnagram {
-    public static void main(String[] args){
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length() ){
+            System.out.println("testing testing");
+            return false;
+        }
+
+        //brute force -> sort the strings and make sure they are equal
+        // char[] a = s.toCharArray();
+        // char[] b = t.toCharArray();
+
+        // Arrays.sort(a);
+        // Arrays.sort(b);
+
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // return Arrays.equals(a, b);
+
+        HashMap<Character, Integer> counter = new HashMap<>();
+
+        // stores in counter the amount of times each letter appears in string s
+        for( char ptr : s.toCharArray()){
+            counter.put(ptr, counter.getOrDefault(ptr,0) + 1);
+        }
+
+        for( char ptr : t.toCharArray()){
+            if(!counter.containsKey(ptr)){ //if theres a char in t that is not in string s then its automatically false
+                return false;
+            }
+            counter.put(ptr, counter.get(ptr) - 1);
+            if(counter.get(ptr) < 0){ //if this is less than 0 then that means this char appears more times in t than s
+                return false;
+            }
+        }
+
+        //all values in counter for all the keys need to be zero or else they are not exactly the same
+        for(int count : counter.values()){
+            if(count != 0) return false;
+        }
+
+        return true;
 
     }
 }
